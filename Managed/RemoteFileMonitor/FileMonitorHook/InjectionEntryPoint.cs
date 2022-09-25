056e8e3287da6cd8217fb89399200b3eaaa5599b
Jesse Fish <jesfish+git@gmail.com>
2022-09-25 00:32:10 -0700

    Make injector trigger on xinput state reads

--- Managed/RemoteFileMonitor/FileMonitorHook/InjectionEntryPoint.cs
+++ Managed/RemoteFileMonitor/FileMonitorHook/InjectionEntryPoint.cs
@@ -30,6 +30,98 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
+[StructLayout(LayoutKind.Explicit)]
+internal struct XInputState {
+    /// <summary>
+    /// The PacketNumber.
+    /// </summary>
+    [FieldOffset(0)] public int PacketNumber;
+
+    /// <summary>
+    /// The Gamepad.
+    /// </summary>
+    [FieldOffset(4)] public XInputGamepad Gamepad;
+
+    /// <summary>
+    /// Copies the source.
+    /// </summary>
+    /// <param name="source">The Source.</param>
+    public void Copy(XInputState source) {
+        PacketNumber = source.PacketNumber;
+        Gamepad.Copy(source.Gamepad);
+    }
+}
+[StructLayout(LayoutKind.Explicit)]
+internal struct XInputGamepad {
+    /// <summary>
+    /// The Buttons.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I2)] [FieldOffset(0)] public short wButtons;
+
+    /// <summary>
+    /// The back left trigger.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I1)] [FieldOffset(2)] public byte bLeftTrigger;
+
+    /// <summary>
+    /// The back right trigger.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I1)] [FieldOffset(3)] public byte bRightTrigger;
+
+    /// <summary>
+    /// The thumb left X.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I2)] [FieldOffset(4)] public short sThumbLX;
+
+    /// <summary>
+    /// The thumb left Y.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I2)] [FieldOffset(6)] public short sThumbLY;
+
+    /// <summary>
+    /// The thumb right X.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I2)] [FieldOffset(8)] public short sThumbRX;
+
+    /// <summary>
+    /// The thumb right Y.
+    /// </summary>
+    [MarshalAs(UnmanagedType.I2)] [FieldOffset(10)] public short sThumbRY;
+
+    /// <summary>
+    /// A value indicating whether the button was pressed.
+    /// </summary>
+    /// <param name="buttonFlags">The ButtonFlags.</param>
+    /// <returns>True if the button was pressed.</returns>
+    public bool IsButtonPressed(int buttonFlags) {
+        return (wButtons & buttonFlags) != 0;
+    }
+
+    /// <summary>
+    /// A value indicating whether the button is accessable on the gamepad.
+    /// </summary>
+    /// <param name="buttonFlags">The ButtonFlags.</param>
+    /// <returns>True if accessable.</returns>
+    public bool IsButtonPresent(int buttonFlags) {
+        return (wButtons & buttonFlags) == buttonFlags;
+    }
+
+    /// <summary>
+    /// Copies the source.
+    /// </summary>
+    /// <param name="source">The Source.</param>
+    public void Copy(XInputGamepad source) {
+        sThumbLX = source.sThumbLX;
+        sThumbLY = source.sThumbLY;
+        sThumbRX = source.sThumbRX;
+        sThumbRY = source.sThumbRY;
+        bLeftTrigger = source.bLeftTrigger;
+        bRightTrigger = source.bRightTrigger;
+        wButtons = source.wButtons;
+    }
+}
+
 namespace FileMonitorHook
 {
     /// <summary>
@@ -82,7 +174,7 @@ namespace FileMonitorHook
             _server.IsInstalled(EasyHook.RemoteHooking.GetCurrentProcessId());
 
             // Install hooks
-
+            /*
             // CreateFile https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx
             var createFileHook = EasyHook.LocalHook.Create(
                 EasyHook.LocalHook.GetProcAddress("kernel32.dll", "CreateFileW"),
@@ -99,12 +191,16 @@ namespace FileMonitorHook
             var writeFileHook = EasyHook.LocalHook.Create(
                 EasyHook.LocalHook.GetProcAddress("kernel32.dll", "WriteFile"),
                 new WriteFile_Delegate(WriteFile_Hook),
-                this);
+                this);*/
+            var readInputHook = EasyHook.LocalHook.Create(
+               EasyHook.LocalHook.GetProcAddress("xinput1_3.dll", "XInputGetState"),
+               new XInputGetState_Delegate(XInputGetState_Hook),
+               this);
+
+
 
             // Activate hooks on all threads except the current thread
-            createFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
-            readFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
-            writeFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+            readInputHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
 
             _server.ReportMessage("CreateFile, ReadFile and WriteFile hooks installed");
 
@@ -143,30 +239,27 @@ namespace FileMonitorHook
             }
 
             // Remove hooks
-            createFileHook.Dispose();
-            readFileHook.Dispose();
-            writeFileHook.Dispose();
+            readInputHook.Dispose();
 
             // Finalise cleanup of hooks
             EasyHook.LocalHook.Release();
         }
 
         /// <summary>
-        /// P/Invoke to determine the filename from a file handle
-        /// https://msdn.microsoft.com/en-us/library/windows/desktop/aa364962(v=vs.85).aspx
+        /// Gets the XInput state.
         /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpszFilePath"></param>
-        /// <param name="cchFilePath"></param>
-        /// <param name="dwFlags"></param>
+        /// <param name="dwUserIndex">The Index.</param>
+        /// <param name="pState">The InputState.</param>
         /// <returns></returns>
-        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        static extern uint GetFinalPathNameByHandle(IntPtr hFile, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpszFilePath, uint cchFilePath, uint dwFlags);
-
-        #region CreateFileW Hook
+        [DllImport("xinput1_3.dll")]
+        internal static extern int XInputGetState
+            (
+            int dwUserIndex,
+            ref XInputState pState
+            );
 
         /// <summary>
-        /// The CreateFile delegate, this is needed to create a delegate of our hook function <see cref="CreateFile_Hook(string, uint, uint, IntPtr, uint, uint, IntPtr)"/>.
+        /// The CreateFile delegate, this is needed to create a delegate of our hook function <see cref=" XInputGetState_hook(int, ref XInputState )"/>.
         /// </summary>
         /// <param name="filename"></param>
         /// <param name="desiredAccess"></param>
@@ -179,38 +272,9 @@ namespace FileMonitorHook
         [UnmanagedFunctionPointer(CallingConvention.StdCall,
                     CharSet = CharSet.Unicode,
                     SetLastError = true)]
-        delegate IntPtr CreateFile_Delegate(
-                    String filename,
-                    UInt32 desiredAccess,
-                    UInt32 shareMode,
-                    IntPtr securityAttributes,
-                    UInt32 creationDisposition,
-                    UInt32 flagsAndAttributes,
-                    IntPtr templateFile);
-
-        /// <summary>
-        /// Using P/Invoke to call original method.
-        /// https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="desiredAccess"></param>
-        /// <param name="shareMode"></param>
-        /// <param name="securityAttributes"></param>
-        /// <param name="creationDisposition"></param>
-        /// <param name="flagsAndAttributes"></param>
-        /// <param name="templateFile"></param>
-        /// <returns></returns>
-        [DllImport("kernel32.dll",
-            CharSet = CharSet.Unicode,
-            SetLastError = true, CallingConvention = CallingConvention.StdCall)]
-        static extern IntPtr CreateFileW(
-            String filename,
-            UInt32 desiredAccess,
-            UInt32 shareMode,
-            IntPtr securityAttributes,
-            UInt32 creationDisposition,
-            UInt32 flagsAndAttributes,
-            IntPtr templateFile);
+        delegate int XInputGetState_Delegate(
+                    int dwUserIndex,
+                    ref XInputState pState);
 
         /// <summary>
         /// The CreateFile hook function. This will be called instead of the original CreateFile once hooked.
@@ -223,238 +287,30 @@ namespace FileMonitorHook
         /// <param name="flagsAndAttributes"></param>
         /// <param name="templateFile"></param>
         /// <returns></returns>
-        IntPtr CreateFile_Hook(
-            String filename,
-            UInt32 desiredAccess,
-            UInt32 shareMode,
-            IntPtr securityAttributes,
-            UInt32 creationDisposition,
-            UInt32 flagsAndAttributes,
-            IntPtr templateFile)
-        {
-            try
-            {
-                lock (this._messageQueue)
-                {
-                    if (this._messageQueue.Count < 1000)
-                    {
-                        string mode = string.Empty;
-                        switch (creationDisposition)
-                        {
-                            case 1:
-                                mode = "CREATE_NEW";
-                                break;
-                            case 2:
-                                mode = "CREATE_ALWAYS";
-                                break;
-                            case 3:
-                                mode = "OPEN_ALWAYS";
-                                break;
-                            case 4:
-                                mode = "OPEN_EXISTING";
-                                break;
-                            case 5:
-                                mode = "TRUNCATE_EXISTING";
-                                break;
-                        }
+        int XInputGetState_Hook(
+                    int dwUserIndex,
+                    ref XInputState pState) {
+            try {
+                lock (this._messageQueue) {
+                    if (this._messageQueue.Count < 1000) {
+                        string user = dwUserIndex.ToString();
 
                         // Add message to send to FileMonitor
                         this._messageQueue.Enqueue(
-                            string.Format("[{0}:{1}]: CREATE ({2}) \"{3}\"",
-                            EasyHook.RemoteHooking.GetCurrentProcessId(), EasyHook.RemoteHooking.GetCurrentThreadId()
-                            , mode, filename));
+                            string.Format("[{0}:{1}]: read input for user ({2})",
+                            EasyHook.RemoteHooking.GetCurrentProcessId(),
+                            EasyHook.RemoteHooking.GetCurrentThreadId(),
+                            user));
                     }
                 }
             }
-            catch
-            {
+            catch {
                 // swallow exceptions so that any issues caused by this code do not crash target process
             }
 
             // now call the original API...
-            return CreateFileW(
-                filename,
-                desiredAccess,
-                shareMode,
-                securityAttributes,
-                creationDisposition,
-                flagsAndAttributes,
-                templateFile);
+            return XInputGetState(dwUserIndex, ref pState);
         }
 
-        #endregion
-
-        #region ReadFile Hook
-
-        /// <summary>
-        /// The ReadFile delegate, this is needed to create a delegate of our hook function <see cref="ReadFile_Hook(IntPtr, IntPtr, uint, out uint, IntPtr)"/>.
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToRead"></param>
-        /// <param name="lpNumberOfBytesRead"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        [UnmanagedFunctionPointer(CallingConvention.StdCall, SetLastError = true)]
-        delegate bool ReadFile_Delegate(
-            IntPtr hFile,
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToRead,
-            out uint lpNumberOfBytesRead,
-            IntPtr lpOverlapped);
-
-        /// <summary>
-        /// Using P/Invoke to call the orginal function
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToRead"></param>
-        /// <param name="lpNumberOfBytesRead"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.StdCall)]
-        static extern bool ReadFile(
-            IntPtr hFile, 
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToRead, 
-            out uint lpNumberOfBytesRead, 
-            IntPtr lpOverlapped);
-
-        /// <summary>
-        /// The ReadFile hook function. This will be called instead of the original ReadFile once hooked.
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToRead"></param>
-        /// <param name="lpNumberOfBytesRead"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        bool ReadFile_Hook(
-            IntPtr hFile,
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToRead,
-            out uint lpNumberOfBytesRead,
-            IntPtr lpOverlapped)
-        {
-            bool result = false;
-            lpNumberOfBytesRead = 0;
-
-            // Call original first so we have a value for lpNumberOfBytesRead
-            result = ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, out lpNumberOfBytesRead, lpOverlapped);
-
-            try
-            {
-                lock (this._messageQueue)
-                {
-                    if (this._messageQueue.Count < 1000)
-                    {
-                        // Retrieve filename from the file handle
-                        StringBuilder filename = new StringBuilder(255);
-                        GetFinalPathNameByHandle(hFile, filename, 255, 0);
-
-                        // Add message to send to FileMonitor
-                        this._messageQueue.Enqueue(
-                            string.Format("[{0}:{1}]: READ ({2} bytes) \"{3}\"",
-                            EasyHook.RemoteHooking.GetCurrentProcessId(), EasyHook.RemoteHooking.GetCurrentThreadId()
-                            , lpNumberOfBytesRead, filename));
-                    }
-                }
-            }
-            catch
-            {
-                // swallow exceptions so that any issues caused by this code do not crash target process
-            }
-
-            return result;
-        }
-
-        #endregion
-
-        #region WriteFile Hook
-
-        /// <summary>
-        /// The WriteFile delegate, this is needed to create a delegate of our hook function <see cref="WriteFile_Hook(IntPtr, IntPtr, uint, out uint, IntPtr)"/>.
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToWrite"></param>
-        /// <param name="lpNumberOfBytesWritten"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        delegate bool WriteFile_Delegate(
-            IntPtr hFile,
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToWrite,
-            out uint lpNumberOfBytesWritten,
-            IntPtr lpOverlapped);
-
-        /// <summary>
-        /// Using P/Invoke to call original WriteFile method
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToWrite"></param>
-        /// <param name="lpNumberOfBytesWritten"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true, CallingConvention = CallingConvention.StdCall)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool WriteFile(
-            IntPtr hFile,
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToWrite,
-            out uint lpNumberOfBytesWritten,
-            IntPtr lpOverlapped);
-
-        /// <summary>
-        /// The WriteFile hook function. This will be called instead of the original WriteFile once hooked.
-        /// </summary>
-        /// <param name="hFile"></param>
-        /// <param name="lpBuffer"></param>
-        /// <param name="nNumberOfBytesToWrite"></param>
-        /// <param name="lpNumberOfBytesWritten"></param>
-        /// <param name="lpOverlapped"></param>
-        /// <returns></returns>
-        bool WriteFile_Hook(
-            IntPtr hFile,
-            IntPtr lpBuffer,
-            uint nNumberOfBytesToWrite,
-            out uint lpNumberOfBytesWritten,
-            IntPtr lpOverlapped)
-        {
-            bool result = false;
-
-            // Call original first so we get lpNumberOfBytesWritten
-            result = WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, lpOverlapped);
-
-            try
-            {
-                lock (this._messageQueue)
-                {
-                    if (this._messageQueue.Count < 1000)
-                    {
-                        // Retrieve filename from the file handle
-                        StringBuilder filename = new StringBuilder(255);
-                        GetFinalPathNameByHandle(hFile, filename, 255, 0);
-
-                        // Add message to send to FileMonitor
-                        this._messageQueue.Enqueue(
-                            string.Format("[{0}:{1}]: WRITE ({2} bytes) \"{3}\"",
-                            EasyHook.RemoteHooking.GetCurrentProcessId(), EasyHook.RemoteHooking.GetCurrentThreadId()
-                            , lpNumberOfBytesWritten, filename));
-                    }
-                }
-            }
-            catch
-            {
-                // swallow exceptions so that any issues caused by this code do not crash target process
-            }
-
-            return result;
-        }
-
-        #endregion
     }
 }
